dff13d6a119c8bb70f47497ce0d7b849360ef5c9
si3nloong <sianloong90@gmail.com>
2025-07-01 01:07:48 +0800

    fix: middleware

--- ocpi2.2.1/middleware.go
+++ ocpi2.2.1/middleware.go
@@ -13,14 +13,7 @@ func (s *Server) authorizeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
-		authorization := strings.TrimSpace(r.Header.Get("Authorization"))
-		paths := strings.SplitN(authorization, " ", 2)
-		if len(paths) != 2 {
-			httputil.ResponseError(w, fmt.Errorf(`missing "Authorization" header`), ocpi.StatusCodeServerError)
-			return
-		}
-
-		token := strings.TrimSpace(paths[1])
+		token := strings.TrimSpace(r.Header.Get("Authorization"))
 		if !s.credentials.VerifyToken(r.Context(), token) {
 			httputil.ResponseError(w, fmt.Errorf(`invalid token`), ocpi.StatusCodeClientError)
 			return
