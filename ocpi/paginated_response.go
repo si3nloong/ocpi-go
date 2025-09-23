66116933eebf98f133b24a152eeb805522c7bdd6
si3nloong <sianloong90@gmail.com>
2025-09-23 19:11:37 +0800

    chore: add new function to construct `PaginatedResponse`

---
+++ ocpi/paginated_response.go
@@ -0,0 +1,62 @@
+package ocpi
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type PaginatedResponse[T any] struct {
+	headers       http.Header
+	RawData       json.RawMessage `json:"data,omitempty"`
+	StatusCode    StatusCode      `json:"status_code"`
+	StatusMessage string          `json:"status_message,omitempty,omitzero"`
+	Timestamp     time.Time       `json:"timestamp"`
+}
+
+func (r *PaginatedResponse[T]) Data() ([]T, error) {
+	var o []T
+	if r.StatusCode >= 1_000 && r.StatusCode < 2_000 {
+		if len(r.RawData) == 0 {
+			return o, nil
+		}
+		if err := json.Unmarshal(r.RawData, &o); err != nil {
+			return o, err
+		}
+		return o, nil
+	}
+	return o, NewOCPIError(r.StatusCode, r.StatusMessage)
+}
+
+func (r *PaginatedResponse[T]) ScanHeaders(headers http.Header) error {
+	r.headers = headers.Clone()
+	return nil
+}
+
+func (r *PaginatedResponse[T]) Link() string {
+	return r.headers.Get(HttpHeaderLink)
+}
+
+func (r *PaginatedResponse[T]) TotalCount() (int, error) {
+	return strconv.Atoi(r.headers.Get(HttpHeaderXTotalCount))
+}
+
+func (r *PaginatedResponse[T]) Limit() (int, error) {
+	return strconv.Atoi(r.headers.Get(HttpHeaderXLimit))
+}
+
+func NewPaginatedResponse[T any](link string, limit, totalCount int, v []T) *PaginatedResponse[T] {
+	b, _ := json.Marshal(v)
+	headers := make(http.Header)
+	headers.Add(HttpHeaderXLimit, strconv.Itoa(limit))
+	headers.Add(HttpHeaderLink, link)
+	headers.Add(HttpHeaderXTotalCount, strconv.Itoa(totalCount))
+	return &PaginatedResponse[T]{
+		headers:       headers,
+		RawData:       b,
+		StatusCode:    StatusCodeSuccess,
+		StatusMessage: StatusCodeSuccess.String(),
+		Timestamp:     time.Now().UTC(),
+	}
+}
--- ocpi/response.go
+++ ocpi/response.go
@@ -2,8 +2,6 @@ package ocpi
 
 import (
 	"encoding/json"
-	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -51,43 +49,4 @@ func NewEmptyResponse() *Response[any] {
 		StatusMessage: StatusCodeSuccess.String(),
 		Timestamp:     time.Now().UTC(),
 	}
-}
-
-type PaginatedResponse[T any] struct {
-	headers       http.Header
-	RawData       json.RawMessage `json:"data,omitempty"`
-	StatusCode    StatusCode      `json:"status_code"`
-	StatusMessage string          `json:"status_message,omitempty,omitzero"`
-	Timestamp     time.Time       `json:"timestamp"`
-}
-
-func (r *PaginatedResponse[T]) Data() ([]T, error) {
-	var o []T
-	if r.StatusCode >= 1_000 && r.StatusCode < 2_000 {
-		if len(r.RawData) == 0 {
-			return o, nil
-		}
-		if err := json.Unmarshal(r.RawData, &o); err != nil {
-			return o, err
-		}
-		return o, nil
-	}
-	return o, NewOCPIError(r.StatusCode, r.StatusMessage)
-}
-
-func (r *PaginatedResponse[T]) ScanHeaders(headers http.Header) error {
-	r.headers = headers.Clone()
-	return nil
-}
-
-func (r *PaginatedResponse[T]) Link() string {
-	return r.headers.Get(HttpHeaderLink)
-}
-
-func (r *PaginatedResponse[T]) TotalCount() (int, error) {
-	return strconv.Atoi(r.headers.Get(HttpHeaderXTotalCount))
-}
-
-func (r *PaginatedResponse[T]) Limit() (int, error) {
-	return strconv.Atoi(r.headers.Get(HttpHeaderXLimit))
 }
